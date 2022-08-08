07ad534a87b480084fea915007e13714794e77e2
julioxbs <julioxbs@gmail.com>
2022-08-08 12:28:54 +0100

    style hover

--- src/components/CompleteForm.vue
+++ src/components/CompleteForm.vue
@@ -4,7 +4,7 @@ const reloadPage = () => window.location.reload();
 
 <template>
     <div class="flex" style="flex-direction: column; gap: 1rem;">
-        <img src="images/icon-complete.svg" alt="icon complete">
+        <img src="../../public/images/icon-complete.svg" alt="icon complete">
         <h1>Thank you!</h1>
         <p>We've added your card details</p>
         <button @click="reloadPage">Continue</button>
